2c05caa79d61a1237580fb3e8179ece777edf1ab
Gunders <bjorn-niklas@hotmail.com>
2020-09-02 22:10:58 +0200

    Duplication code for Modal rendering is now removed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import {fetchStationStatus, fetchStationInformation} from "./Api/FetchStations";
+import React, {useEffect, useState} from 'react';
+import {fetchStationInformation, fetchStationStatus} from "./Api/FetchStations";
 import './App.css';
 import Station from './StationComponents/Station.js';
 import Row from 'react-bootstrap/Row'
@@ -12,7 +12,7 @@ const App = () => {
   //its default value is a Map object, to enable directly inserting values based on ID(due to constant status calls)
   const [stations, setStations] = useState(new Map());
   const [show, setShow] = useState(false);
-  const [modalStation, setModalStation] = useState();
+  const [modalStation, setModalStation] = useState("");
   const [pageNumber, setPageNUmber] = useState(1);
   //Change this for amount of divs per page
   const numPerPage = 9;
@@ -80,7 +80,7 @@ const App = () => {
   //Best solution i could come up with on the spot for enabling column/row control
   // without refactoring the component structure.
   function splitArray (array,length) {
-    let newArray = array.reduce(
+    return array.reduce(
         (result, item, index) => {
           if (index % length === 0) result.push([]);
           result[Math.floor(index / length)].push(item);
@@ -88,14 +88,12 @@ const App = () => {
         },
         []
     );
-    return newArray;
   }
   function renderStations(){
     const stationsList = [];
     stations.forEach(function(val){
       stationsList.push(val);
     });
-    //9=amount of stations desired per page
     const numPages = Math.ceil(stationsList.length/numPerPage);
     let items = [];
     for (let number = 1; number <= numPages; number++) {
@@ -128,64 +126,17 @@ const App = () => {
                 ))
               }
             </Container>
-        </div>
-    )
-  }
-  //TODO remove this functions code duplication from renderStations
-  function renderModal(){
-    const stationsList = [];
-    stations.forEach(function(val){
-      stationsList.push(val);
-    });
-    //9=amount of stations desired per page
-    const numPages = Math.ceil(stationsList.length/numPerPage);
-    let items = [];
-    for (let number = 1; number <= numPages; number++) {
-      items.push(
-          <Pagination.Item key={number} id={number} active={number === pageNumber} onClick={changePage.bind(this,number)}>
-            {number}
-          </Pagination.Item>
-      );
-    }
-    console.log(numPages);
-    let pageChunks = splitArray(stationsList,numPerPage);
-    console.log(pageChunks);
-    return(
-        <div className="App">
-          <Container fluid>
-            <div>
-              <Row>
-                {items.map(page => (
-                    <Pagination>{page}</Pagination>
-                ))}
-              </Row>
-            </div>
-            {
-              splitArray(pageChunks[pageNumber],3).map(stationChunk => (
-                  <Row>
-                    {stationChunk.map(s => <Station key={s.id} name={s.name} passFunction={openHandler.bind(this, s)} address={s.address} is_renting={s.is_renting}
-                                                    is_returning={s.is_returning} num_bikes_available={s.num_bikes_available}
-                                                    num_docks_available={s.num_docks_available} column={s.column}/>)}
-                  </Row>
-              ))
-            }
-          </Container>
           <StationModal key={modalStation.id} show = {show} name={modalStation.name} passFunction={closeHandler.bind(this)} address={modalStation.address} is_renting={modalStation.is_renting}
                         is_returning={modalStation.is_returning} num_bikes_available={modalStation.num_bikes_available}
                         num_docks_available={modalStation.num_docks_available} column={modalStation.column}/>
         </div>
-    );
+    )
   }
   //You could also do return({stations.size>0?<Station /> : <p>Loading..</p>}),
   // however i personally prefer the use of basic if else in terms of code structure.
   if(stations.size>0) {
     //Unexpected complexity arose from directly attempting to use a Map object within this render function, so
     //a Array is created from it as a fix.
-    if(show){
-      return(
-          renderModal()
-      )
-    }
     return (
         renderStations()
     )
