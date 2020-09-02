95ca259bbf8772ff20cc4b83266f2464f0ba12a7
Gunders <bjorn-niklas@hotmail.com>
2020-09-02 18:56:52 +0200

    Added StationModal, Needs styling

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 import Station from './StationComponents/Station.js';
 import Row from 'react-bootstrap/Row'
 import Container from 'react-bootstrap/Container'
+import StationModal from "./StationComponents/StationModal";
 
 const App = () => {
   //Note to self setStations causes a rerender
   //its default value is a Map object, to enable directly inserting values based on ID(due to constant status calls)
   const [stations, setStations] = useState(new Map());
 
+  const [show, setShow] = useState(false);
+  const [modalStation, setModalStation] = useState();
 
   //call get station info once. (This is intended to replace ComponentDidMount, and exclude DidUpdate.)
   //then it will start a interval of 10 seconds to try and update each stations information.
@@ -37,7 +40,6 @@ const App = () => {
 
   const renewStationStatus = async () => {
     let gottenStationsStatus = await fetchStationStatus();
-    console.log("called fetchStatus");
     updateStations(gottenStationsStatus.data.stations)
   };
   //This function is separated to isolate the update Routine
@@ -60,52 +62,90 @@ const App = () => {
     setStations(stationsToUpdate);
   }
   //This only demonstrates the sending of events to the child component working.
-  function clickHandler(station){
+  function openHandler(station){
     console.log(station);
+    setModalStation(station);
+    setShow(true);
   }
-  //You could also do return({stations.size>0?<Station /> : <p>Loading..</p>}),
-  // however i personally prefer the use of basic if else in terms of code structure.
-  if(stations.size>0){
-    //Unexpected complexity arose from directly attempting to use a Map object within this render function, so
-    //a Array is created from it as a fix.
+  function closeHandler(){
+    setShow(false);
+  }
+  function renderStations(){
     const stationsList = [];
     stations.forEach(function(val){
       stationsList.push(val);
     });
-    //Best solution i could come up with on the spot for enabling column/row control
-    // without refactoring the component structure.
-    const splitArray = (array, length) =>
-        array.reduce(
-            (result, item, index) => {
-              if ( index % length === 0 ) result.push([]);
-              result[Math.floor(index / length)].push(item);
-              return result
-            },
-            []
-        );
-    return (
+    return(
         <div className="App">
           <Container fluid>
-          {
-            splitArray(stationsList,3).map(stationChunk => (
-                <Row fluid="true">
-                  {stationChunk.map(s => <Station key={s.id} name={s.name} passFunction={clickHandler.bind(this, s)} address={s.address} is_renting={s.is_renting}
-                                                      is_returning={s.is_returning} num_bikes_available={s.num_bikes_available}
-                                                      num_docks_available={s.num_docks_available} column={s.column}/>)}
-                </Row>
-            ))
-          }
+            {
+              splitArray(stationsList,3).map(stationChunk => (
+                  <Row fluid="true">
+                    {stationChunk.map(s => <Station key={s.id} name={s.name} passFunction={openHandler.bind(this, s)} address={s.address} is_renting={s.is_renting}
+                                                    is_returning={s.is_returning} num_bikes_available={s.num_bikes_available}
+                                                    num_docks_available={s.num_docks_available} column={s.column}/>)}
+                  </Row>
+              ))
+            }
           </Container>
         </div>
+    );
+  }
+  //TODO remove the associated code duplication from renderStations
+  function renderModal(){
+    const stationsList = [];
+    stations.forEach(function(val){
+      stationsList.push(val);
+    });
+    return(
+        <div className="App">
+          <Container fluid>
+            {
+              splitArray(stationsList,3).map(stationChunk => (
+                  <Row fluid="true">
+                    {stationChunk.map(s => <Station key={s.id} name={s.name} passFunction={openHandler.bind(this, s)} address={s.address} is_renting={s.is_renting}
+                                                    is_returning={s.is_returning} num_bikes_available={s.num_bikes_available}
+                                                    num_docks_available={s.num_docks_available} column={s.column}/>)}
+                  </Row>
+              ))
+            }
+          </Container>
+          <StationModal key={modalStation.id} show = {show} name={modalStation.name} passFunction={closeHandler.bind(this)} address={modalStation.address} is_renting={modalStation.is_renting}
+                        is_returning={modalStation.is_returning} num_bikes_available={modalStation.num_bikes_available}
+                        num_docks_available={modalStation.num_docks_available} column={modalStation.column}/>
+        </div>
+    );
+  }
+  //Best solution i could come up with on the spot for enabling column/row control
+  // without refactoring the component structure.
+  const splitArray = (array, length) =>
+      array.reduce(
+          (result, item, index) => {
+            if ( index % length === 0 ) result.push([]);
+            result[Math.floor(index / length)].push(item);
+            return result
+          },
+          []
+      );
+  //You could also do return({stations.size>0?<Station /> : <p>Loading..</p>}),
+  // however i personally prefer the use of basic if else in terms of code structure.
+  if(stations.size>0) {
+    //Unexpected complexity arose from directly attempting to use a Map object within this render function, so
+    //a Array is created from it as a fix.
+    if(show){
+      return(
+          renderModal()
+      )
+    }
+    return (
+        renderStations()
     )
   }
-  else{
     return (
         // implement loading component logic here.
           <div className="app-loading">
             <p>Loading....</p>
           </div>
     )
-  }
 };
 export default App;
---
+++ src/StationComponents/StationModal.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './StationModal.css';
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import {Bicycle, ConeStriped} from "react-bootstrap-icons";
+
+//Will spawn a Modal when a station is clicked, with its information within it.
+//Do note that this throws a deprecated warning, however that is from the react-bootstrap modal itself.
+const StationModal = props => {
+    //Code related to adapting the messages shown on component based on props values
+    //TODO Remove the associated code duplication from Station.js
+    let message="";
+    let rentMessage=""+props.num_bikes_available + " Ledige Sykler";
+    let parkMessage=props.num_docks_available +" Ledige Plasser";
+    if(!props.is_renting){
+        message = message.concat("Du kan ikke leie her for Øyeblikket.");
+    }
+    if(!props.is_returning){
+        message = message.concat("Du kan ikke parkere her for Øyeblikket.");
+    }
+    if(props.num_docks_available===0){
+        parkMessage = "Ingen ledige Plasser";
+    }
+    if(props.num_bikes_available===0){
+        rentMessage = "Ingen ledige Sykler";
+    }
+    return (
+        <>
+            <Modal show={props.show} onHide={props.passFunction}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{props.name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Bicycle/><p>{rentMessage}</p>
+                    <ConeStriped/><p>{parkMessage}</p>
+                    <p>{message}</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={props.passFunction}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+export default StationModal;
